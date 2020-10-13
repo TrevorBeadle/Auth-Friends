f7756bf94faf3cb9e801991448bb8f821bdbca2d
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-10-13 15:34:16 -0500

    finished friend form and added it to app with friends list

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import Friend from "./Friend";
+import FriendForm from "./FriendForm";
 
 class FriendsList extends React.Component {
   state = {
@@ -20,6 +21,18 @@ class FriendsList extends React.Component {
       .catch(err => console.log(err));
   };
 
+  onSubmit = data => {
+    axiosWithAuth()
+      .post("/api/friends", data)
+      .then(res => {
+        this.setState({
+          ...this.state,
+          friends: res.data,
+        });
+      })
+      .catch(err => console.log(err));
+  };
+
   componentDidMount() {
     this.getFriends();
   }
@@ -28,8 +41,9 @@ class FriendsList extends React.Component {
     return (
       <div className="friends">
         {this.state.friends.map(friend => {
-          return <Friend friend={friend} />;
+          return <Friend key={friend.id} friend={friend} />;
         })}
+        <FriendForm onSubmit={this.onSubmit} />
       </div>
     );
   }
