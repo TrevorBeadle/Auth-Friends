3a9f62ff46d27b7d2563aa6e17e7f1ed064d2aea
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-10-13 18:08:45 -0500

    wrapped friend names in links to their inidividual friend pages

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Friend({ friend }) {
   return (
     <div className="friend">
-      <h3>Name: {friend.name}</h3>
+      <Link to={`/friends/${friend.id}`}>
+        <h3>Name: {friend.name}</h3>
+      </Link>
       <h5>Age: {friend.age}</h5>
       <h6>Email: {friend.email}</h6>
     </div>
